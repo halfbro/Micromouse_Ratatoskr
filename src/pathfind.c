4d1fc1c16bffd2203990f2f7b3ba925856cb2609
halfbro <mlagaren@stevens.edu>
2017-03-22 13:55:51 -0400

    Start pathfinding algorithm; Generates the previous cell map

--- src/pathfind.c
+++ src/pathfind.c
@@ -1,6 +1,12 @@
 #include "pathfind.h"
 #include "structures.h"
 
+#define DIR_NONE  0
+#define DIR_NORTH 1
+#define DIR_EAST  2
+#define DIR_SOUTH 3
+#define DIR_WEST  4
+
 void resetSearchBit(Maze* maze) {
   for (int i=0; i<16; ++i) {
     for (int j=0; j<16; ++j) {
@@ -11,7 +17,59 @@ void resetSearchBit(Maze* maze) {
 
 // Assumes knowledge of the maze is known
 void pathfind(Maze* maze, uint8_t x, uint8_t y, uint8_t tx, uint8_t ty, Path* output) {
-  int i = 0;
+  // Floodfill (BFS) while keeping track of previous cells
+  uint8_t nextcell[32];
+  int head=-1, tail=-1;
+  resetSearchBit(maze);
+  Maze prevcelldir;
+
+  nextcell[++tail % 32] = x + y*16;
+  prevcelldir[x][y] = 0;
+  *maze[x][y] |= CELL_SEARCHED;
+
+  uint8_t cellnum = 0;
+  int cx=0, cy=0;
+
+  do {
+    cellnum = nextcell[++head];
+    cx = cellnum % 16;
+    cy = cellnum / 16;
+
+    if ( (*maze[cx][cy] & CELL_WALL_NORTH) == 0 ) {   // If there is no wall up...
+      if ( (*maze[cx][cy+1] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
+        nextcell[++tail % 32] = cx + (cy+1)*16;           // Add the cell to ringbuf...
+        *maze[cx][cy+1] |= CELL_SEARCHED;                 // and set the cell to searched
+        prevcelldir[cx][cy+1] = DIR_SOUTH;
+      }
+    }
+
+    if ( (*maze[cx][cy] & CELL_WALL_EAST) == 0 ) {   // If there is no wall right...
+      if ( (*maze[cx+1][cy] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
+        nextcell[++tail % 32] = (cx+1) + cy*16;           // Add the cell to ringbuf...
+        *maze[cx+1][cy] |= CELL_SEARCHED;                 // and set the cell to searched
+        prevcelldir[cx+1][cy] = DIR_WEST;
+      }
+    }
+
+    if ( (*maze[cx][cy] & CELL_WALL_SOUTH) == 0 ) {   // If there is no wall down...
+      if ( (*maze[cx][cy-1] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
+        nextcell[++tail % 32] = cx + (cy-1)*16;           // Add the cell to ringbuf...
+        *maze[cx][cy-1] |= CELL_SEARCHED;                 // and set the cell to searched
+        prevcelldir[cx][cy-1] = DIR_NORTH;
+      }
+    }
+
+    if ( (*maze[cx][cy] & CELL_WALL_WEST) == 0 ) {   // If there is no wall left...
+      if ( (*maze[cx-1][cy] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
+        nextcell[++tail % 32] = (cx-1) + cy*16;           // Add the cell to ringbuf...
+        *maze[cx-1][cy] |= CELL_SEARCHED;                 // and set the cell to searched
+        prevcelldir[cx-1][cy] = DIR_EAST;
+      }
+    }
+
+  } while ( (cx != tx && cy != ty) && (head != tail) );  // While current cell has been visited...
+
+  // Manipulate the prevcelldir array into a usable path
 }
 
 // For now just call the normal pathfinding function
@@ -19,48 +77,47 @@ void pathfindOptimized(Maze* maze, uint8_t x, uint8_t y, uint8_t tx, uint8_t ty,
   pathfind(maze, x, y, tx, ty, output);
 }
 
-
 uint8_t findClosestUnexploredCell(Maze* maze, uint8_t x, uint8_t y) {
-  uint8_t ringbuf[32]; // Holds cell number for the next cells to check in BFS
+  uint8_t nextcell[32]; // Holds cell number for the next cells to check in BFS
   int head=-1, tail=-1; // Indices used by the ringbuffer
   resetSearchBit(maze);
 
-  ringbuf[++tail % 32] = x + y*16;
+  nextcell[++tail % 32] = x + y*16;
+  *maze[x][y] |= CELL_SEARCHED;
 
   uint8_t cellnum=0;
   int cx=0, cy=0; 
 
   do {
-    cellnum = ringbuf[++head];
+    cellnum = nextcell[++head];
     cx = cellnum % 16;
     cy = cellnum / 16;
-    *maze[cx][cy] |= CELL_SEARCHED;
 
     if ( (*maze[cx][cy] & CELL_WALL_NORTH) == 0 ) {   // If there is no wall up...
       if ( (*maze[cx][cy+1] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
-        ringbuf[++tail % 32] = cx + (cy+1)*16;            // Add the cell to ringbuf...
+        nextcell[++tail % 32] = cx + (cy+1)*16;           // Add the cell to ringbuf...
         *maze[cx][cy+1] |= CELL_SEARCHED;                 // and set the cell to searched
       }
     }
 
     if ( (*maze[cx][cy] & CELL_WALL_EAST) == 0 ) {   // If there is no wall right...
       if ( (*maze[cx+1][cy] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
-        ringbuf[++tail % 32] = (cx+1) + cy*16;            // Add the cell to ringbuf...
+        nextcell[++tail % 32] = (cx+1) + cy*16;           // Add the cell to ringbuf...
         *maze[cx+1][cy] |= CELL_SEARCHED;                 // and set the cell to searched
       }
     }
 
     if ( (*maze[cx][cy] & CELL_WALL_SOUTH) == 0 ) {   // If there is no wall down...
       if ( (*maze[cx][cy-1] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
-        ringbuf[++tail % 32] = cx + (cy-1)*16;            // Add the cell to ringbuf...
+        nextcell[++tail % 32] = cx + (cy-1)*16;           // Add the cell to ringbuf...
         *maze[cx][cy-1] |= CELL_SEARCHED;                 // and set the cell to searched
       }
     }
 
     if ( (*maze[cx][cy] & CELL_WALL_WEST) == 0 ) {   // If there is no wall left...
       if ( (*maze[cx-1][cy] & CELL_SEARCHED) == 0 ) {   // and it hasn't been searched yet...
-        ringbuf[++tail % 32] = (cx-1) + cy*16;            // Add the cell to ringbuf...
-        *maze[cx+1][cy] |= CELL_SEARCHED;                 // and set the cell to searched
+        nextcell[++tail % 32] = (cx-1) + cy*16;           // Add the cell to ringbuf...
+        *maze[cx-1][cy] |= CELL_SEARCHED;                 // and set the cell to searched
       }
     }
 
