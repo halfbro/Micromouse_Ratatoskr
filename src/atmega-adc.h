1b49c9cc18a29927f8f721c4b910480c9feff7a7
Matt Lagarenne <mlagaren@stevens.edu>
2017-03-30 16:11:25 -0400

    Clean up ADC of unused functions

--- src/atmega-adc.h
+++ src/atmega-adc.h
@@ -1,16 +1,8 @@
 #ifndef atmega_adc_h
 #define atmega_adc_h
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <avr/io.h>
 #include <avr/interrupt.h>
-
-// Comment or delete this line to disable interrupt definition.
-// You can still use the "adc_read" function if this is disabled.
-//#define ENABLE_ADC_INT
 
 #define ADC_PRESCALER_2 	0
 #define ADC_PRESCALER_4 	2
@@ -24,13 +16,6 @@ extern "C" {
 #define ADC_VREF_MISC1 	128
 #define ADC_VREF_MISC2 	192
 
-
 uint16_t adc_read(uint8_t prescaler, uint8_t vref, uint8_t pin);
-void adc_start(uint8_t prescaler, uint8_t vref, uint8_t pin_qty, void (*handler)(uint8_t, uint16_t));
-void adc_stop();
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
