cc6025e287f25cbc24b1a5458c0ab66daa5b6089
halfbro <mlagaren@stevens.edu>
2017-04-04 16:34:32 -0400

    Fix IR subsystem

--- src/atmega-adc.c
+++ src/atmega-adc.c
@@ -1,3 +1,5 @@
+#include <util/delay.h>
+
 #include "atmega-adc.h"
 
 uint16_t adc_read(uint8_t prescaler, uint8_t vref, uint8_t pin) {
@@ -5,6 +7,7 @@ uint16_t adc_read(uint8_t prescaler, uint8_t vref, uint8_t pin) {
 	
 	ADCSRA = _BV(ADEN) | _BV(ADSC) | prescaler;
 	while(!(ADCSRA & _BV(ADIF)));
+  _delay_us(400);
 	
 	return (ADCL | (ADCH<<8));
 }
--- src/irsensors.c
+++ src/irsensors.c
@@ -9,7 +9,8 @@
 void initIRSensors() {
   // Set IR Emitters as outputs
   DDRE |= 0b00000011;
-  DIDR0 = 0b11111111;
+
+  DDRF &= ~(0b00001111);
 }
 
 uint16_t readADC(uint8_t sensornum) {
@@ -17,33 +18,17 @@ uint16_t readADC(uint8_t sensornum) {
 }
 
 void readIRSensors(IRSensorData *sensors) {
-  uint16_t templ  = 0;
-  uint16_t templc = 0;
-  uint16_t temprc = 0;
-  uint16_t tempr  = 0;
 
   PORTE |= 0b00000010;
-
-  _delay_us(200);
-
-  templ = readADC(0);
-
-  _delay_us(200);
-
-  templc = readADC(1);
-
+  uint16_t templ  = readADC(0);
+  uint16_t templc = readADC(1);
   PORTE &= ~(0b00000010);
-  _delay_us(200);
+
   PORTE |= 0b00000001;
-  _delay_us(200);
-
-  temprc = readADC(2);
-
-  _delay_us(200);
-
-  tempr = readADC(3);
-
+  uint16_t temprc = readADC(2);
+  uint16_t tempr  = readADC(3);
   PORTE &= ~(0b00000001);
+
   sensors->left = templ;
   sensors->leftcenter = templc;
   sensors->rightcenter = temprc;
